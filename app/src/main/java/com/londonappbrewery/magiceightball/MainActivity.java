83479ccfd1559b68aa1ee30fa1ef23ae46666333
Joshua Matos <76454509+joshua-matos@users.noreply.github.com>
2021-01-19 05:56:26 -1000

    Added static imports and images

--- app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
@@ -1,7 +1,13 @@
 package com.londonappbrewery.magiceightball;
-
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Random;
+
+import static com.londonappbrewery.magiceightball.R.drawable.*;
+import static com.londonappbrewery.magiceightball.R.id.*;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView ballDisplay = findViewById(magicBall1);
+
+        final int[] ballArray = new int[]{
+                ball1,
+                ball2,
+                ball5,
+                ball3,
+                ball4
+        };
+
+        Button askButton = findViewById(R.id.askButton);
+        askButton.setOnClickListener((v) -> {
+            int number = new Random().nextInt(5);
+            ballDisplay.setImageResource(ballArray[number]);
+
+        });
     }
 }
